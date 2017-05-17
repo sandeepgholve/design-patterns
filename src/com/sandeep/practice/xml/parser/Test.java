cc2e57a71f415b90e913f95b9b9d0c6129365c81
sgholve <sandeep.gholve@profitbricks.com>
2017-05-17 17:26:40 +0200

    Java 8 and Design Patterns related examples.

---
+++ src/com/sandeep/practice/xml/parser/Test.java
@@ -0,0 +1,11 @@
+package com.sandeep.practice.xml.parser;
+
+/**
+ * Created by sgholve on 11.04.16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Boolean active = Boolean.TRUE;
+        System.out.println(active.toString());
+    }
+}
